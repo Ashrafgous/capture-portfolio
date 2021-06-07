a712989ebc9b471f13fd87c47531ba2fa4868cce
mohammadgous1719 <mohammadgous1719@gmail.com>
2021-06-07 16:57:04 +0530

    8 Adding React Router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import AboutUs from ".//pages/AboutUs";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWorks from "./pages/OurWorks";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWorks />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
