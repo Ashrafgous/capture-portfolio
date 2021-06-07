e30843f65f9951f8f95ef909f82f8497bd36fd92
mohammadgous1719 <mohammadgous1719@gmail.com>
2021-06-07 14:10:43 +0530

    3. service Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 //Page Component
 import AboutSection from "../components/AboutSection";
+import ServiceSection from "../components/ServicesSection";
 
 function AboutUs() {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServiceSection />
+    </>
   );
 }
 
