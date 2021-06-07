0db2bc739776fd82fe5e990d6ac74d3e62679c5a
mohammadgous1719 <mohammadgous1719@gmail.com>
2021-06-07 14:19:56 +0530

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //Page Component
 import AboutSection from "../components/AboutSection";
 import ServiceSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 function AboutUs() {
   return (
     <>
       <AboutSection />
       <ServiceSection />
+      <FaqSection />
     </>
   );
 }
