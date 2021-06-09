69fc30ed246000e5a60320c8b675620cdc4bc8b7
mohammadgous1719 <mohammadgous1719@gmail.com>
2021-06-09 10:09:06 +0530

    11.  About Section frame motion

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,14 +3,22 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServiceSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../pages/animation";
 
 function AboutUs() {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboutSection />
       <ServiceSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 }
 
